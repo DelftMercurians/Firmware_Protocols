f4e0066390f60be9c5e42fadfc89bec3353fc58c
thettasch <thomas.hettasch@gmail.com>
2023-09-08 17:13:32 +0200

    Modify radio protocol to use CAN variables in conf

--- radio/protocols_radio.h
+++ radio/protocols_radio.h
@@ -4,6 +4,8 @@
 // thomas.hettasch@gmail.com
 
 #pragma once
+#include "../can/protocols_can.h"
+
 namespace Radio {
 
 // Statuses that the robot or motor controller MCU's could have
@@ -27,23 +29,18 @@ enum class ConfigMessageType {
     NONE,                   // Don't do anything with this
     SET_DEFAULT,            // Set a parameter to defaults
     SET_DEFAULT_RETURN,     // Reply stating value set to defaults
-    READ,                   // Read a parameter from the robot
-    READ_RETURN,            // Robot return message with value
-    WRITE,                  // Write a parameter to the robot
+    READ = (int) CAN::ACCESS::READ,                   // Read a parameter from the robot
+    READ_RETURN = (int) CAN::ACCESS::MASK,            // Robot return message with value
+    WRITE = (int) CAN::ACCESS::WRITE,                  // Write a parameter to the robot
     WRITE_RETURN,           // Robot return message stating new value
-};
-   
-// Onboard robot configuration variables that can be changed
-enum class ConfigVariabe {
-    PID_CD_P,
-    // ADD MORE HERE
 };
 
 // Configuration message (bidirectional)
 struct ConfigMessage {
     ConfigMessageType mt;   // Message type
-    ConfigVariabe var;      // Variable/Parameter that is being accessed
-    float value;            // Value to be written/that is being acknowledged
+    CAN::VARIABLE var;      // Variable/Parameter that is being accessed
+    CAN_VARIABLE_TYPE value;            // Value to be written/that is being acknowledged
+    uint8_t fluff;          // Just here to change the size of the message
 };
 
 // Command from mothership to robot
