bb670d0f0c723c479ba98aa2da39bdab72bbf43b
thettasch <thomas.hettasch@gmail.com>
2024-04-24 12:06:59 +0200

    Enable 1 or 4 radios on a base station

--- radio/pins_radio.h
+++ radio/pins_radio.h
@@ -15,15 +15,22 @@ struct RadioPins {
 };
 const RadioPins RobotPinMap = { PB0, PA4, SpiBus::Spi_2 };
 
+#ifdef BASE_STATION_MULTI_RADIO
 #define __RADIO_PINS_NUMBER_OF_RADIOS 4
-const uint8_t NumberOfRadios = __RADIO_PINS_NUMBER_OF_RADIOS;
 const RadioPins GroupPinMap[] = {
     { PB0, PA3, SpiBus::Spi_1 },
     { PB11, PA1, SpiBus::Spi_2 },
     { PB1, PA4, SpiBus::Spi_1 },
     { PB10, PA0, SpiBus::Spi_2 },
 };
+#else
+#define __RADIO_PINS_NUMBER_OF_RADIOS 1
+const RadioPins GroupPinMap[] = {
+    { PB0, PA4, SpiBus::Spi_1 },
+};
+#endif
 
+const uint8_t NumberOfRadios = __RADIO_PINS_NUMBER_OF_RADIOS;
 static_assert(NumberOfRadios == (sizeof(GroupPinMap)/sizeof(GroupPinMap[0])));
 
 }
