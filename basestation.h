a18a0810ae3775fc17b1fbfff08722bf3fb84ce6
thettasch <thomas.hettasch@gmail.com>
2024-05-24 12:06:36 +0200

    Add basestation information packet

---
+++ basestation.h
@@ -0,0 +1,19 @@
+// Delft Mercurians
+// Thomas Hettasch
+// 2024-05-24
+// thomas.hettasch@gmail.com
+
+#pragma once
+#include <stdint.h>
+#include "utils.h"
+
+namespace Base {
+
+struct Information {
+    HG::Version version;
+    uint8_t num_radios;
+    uint8_t max_robots;
+    uint16_t radios_online;  // bitfield
+};
+
+}; // namespace Base
