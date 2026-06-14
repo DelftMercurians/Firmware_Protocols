f3ed96797c56d1157723cc68953bf9763c309cbf
Thomas Hettasch <thomas.hettasch@gmail.com>
2026-06-14 13:35:22 +0200

    Intentionally move to new addresses, breaking changes incoming

--- radio/protocols_radio.h
+++ radio/protocols_radio.h
@@ -14,9 +14,9 @@ typedef uint8_t SSL_ID;
 const SSL_ID Broadcast_ID = 0xEE;
 const SSL_ID BaseStation_ID = 0xB0;
 
-const uint64_t BaseAddress_BtR = 0x324867LL;    // Address base to robot
-const uint64_t BaseAddress_RtB = 0x4248A7LL;    // Address robot to bases (LSB must be different enough for uniqueness to kick in)
-const uint64_t BroadcastAddress = 0x3248F7LL;   // Address base to all robots
+const uint64_t BaseAddress_BtR = 0x334867LL;    // Address base to robot
+const uint64_t BaseAddress_RtB = 0x4348A7LL;    // Address robot to bases (LSB must be different enough for uniqueness to kick in)
+const uint64_t BroadcastAddress = 0x3348F7LL;   // Address base to all robots
 
 /* CONFIG MESSAGES */
 
