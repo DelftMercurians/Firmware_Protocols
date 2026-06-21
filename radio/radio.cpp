de9fc91b91b2ca364d42ec8d8a91f7ebb42ad5ff
Alexander Nitters <alex@nitters.eu>
2026-06-21 18:44:08 +0200

    Remove serial prints to fix Firmware_Primary

--- radio/radio.cpp
+++ radio/radio.cpp
@@ -4,10 +4,10 @@
 // Initialise Radio
 void CustomRF24::preInit(rf24_pa_dbm_e pa_level) {
 	this->begin(this->spi);
-    Serial.printf("Initialised Radio, IDENTITY = 0x%X\n", this->identity);
-	Serial.printf("Failure detected: %s\n", this->failureDetected ? "true" : "false");
-	Serial.printf("isChipConnected: %s\n", this->isChipConnected() ? "true" : "false");
-	Serial.printf("isPVariant: %s\n", this->isPVariant() ? "true" : "false");
+    // Serial1.printf("Initialised Radio, IDENTITY = 0x%X\n", this->identity);
+	// Serial1.printf("Failure detected: %s\n", this->failureDetected ? "true" : "false");
+	// Serial1.printf("isChipConnected: %s\n", this->isChipConnected() ? "true" : "false");
+	// Serial1.printf("isPVariant: %s\n", this->isPVariant() ? "true" : "false");
     // this->setPayloadSize(sizeof(Radio::Message));
 	this->setPALevel(pa_level);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
     this->enableDynamicPayloads();
